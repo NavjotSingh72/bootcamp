e86a997a5c4f3b8d0d9f4ddbf6bfd688a885c2c2
NavjotSingh72 <navjotsandhu72917@gmail.com>
2026-07-03 11:52:07 +0530

    check factorial number

---
+++ Machine_learning/Day-3/factorial.py
@@ -0,0 +1,15 @@
+n = int(input("Enter a number: "))
+
+factorial = 1
+
+if n < 0:
+    print("Factorial does not exist for negative numbers.")
+
+elif n == 0:
+    print("Factorial of 0 is 1")
+
+else:
+    for i in range(1, n + 1):
+        factorial = factorial * i
+
+    print(factorial)
